d4ca5909bcf9f84a463ac39658e6b6d0227c3196
Milad Rastian <miladrastian@gmail.com>
2022-05-27 21:10:43 +0200

    use github dep

--- mix.exs
+++ mix.exs
@@ -27,7 +27,7 @@ defmodule ArcticClientMintAdapter.MixProject do
     [
       {:mint, "~> 1.0"},
       {:castore, "~> 0.1.0"},
-      {:arctic_base, "~> 0.1.0"},
+      {:arctic_base, github: "arctic-grpc/arctic-base"},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:mox, "~> 1.0", only: :test}
     ]
